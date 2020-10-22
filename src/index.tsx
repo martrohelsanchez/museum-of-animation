3e3c787dedd3150c7c8713c6bcfa82f6b9b54162
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-22 15:28:49 +0800

    🔥Remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import './cssGlobal.css';
+import App from './components/cardPaging-copy/CardPaging';
+// import App from './components/testing/Testing';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
