44945fad6b9bd0aaaf6eb44a01e02379c9911d9c
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-22 15:27:22 +0800

    💫✨Create card paging animation

---
+++ src/components/cardPaging-copy/CardPaging.styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import {motion} from 'framer-motion';
+
+export const CardPaging = styled.div`
+    background-color: #2d6187;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+`;
+
+export const MobileView = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    width: 411px;
+    height: 731px;
+    border-radius: 30px;
+`;
+
+export const PageCont = styled(motion.div)`
+    display: flex;
+    flex-direction: row;
+    overflow-x: hidden;
+    overflow-y: visible;
+    width: 70%;
+    height: 50%;
+    cursor: grab;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`;
+
+export const Carousel = styled.div`
+    height: 70%;
+`;
+
+export const Page = styled(motion.div)<{bgColor: string}>`
+    background-color: ${({bgColor}) => bgColor};
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    flex: 0 0 auto;
+    border-radius: 20px;
+`;
---
+++ src/components/cardPaging-copy/CardPaging.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import * as S from './CardPaging.styles';
+import {useMotionValue, useTransform, useAnimation, Variants} from 'framer-motion';
+import {PanInfo} from 'framer-motion/types';
+import Page from './Page';
+
+const colors = ['#cc0e74', '#1f3c88', '#a8dda8', '#ff9642', '#ffe05d'];
+
+function CardPaging() {
+    const [currPageIndex, setCurrPageIndex] = useState(0);
+    const [openedPage, setOpenedPage] = useState<number | null>(null);
+    const pageX = useMotionValue(0);
+    const pageAnimation = useAnimation();
+
+    //PageIndex is the current page that is being dragged
+    function onDrag(e: MouseEvent | TouchEvent | PointerEvent, info: PanInfo, pageIndex: number) {
+        // pageX.set(info.offset.x);
+
+        if (info.offset.x < -200 && pageIndex === currPageIndex) {
+            setCurrPageIndex(i => ++i);
+        } else if (info.offset.x > 200 && pageIndex === currPageIndex) {
+            setCurrPageIndex(i => --i);
+        }
+    }
+
+    console.log(openedPage)
+
+    return (
+        <S.CardPaging>
+            <S.MobileView>
+                <S.PageCont
+                    variants={pageContVariant}
+                    animate={openedPage === null ? 'closePage' : 'openPage'}
+                >
+                    {colors.map((color, i) => (
+                        <Page
+                            key={color}
+                            color={color}
+                            i={i}
+                            pageX={pageX}
+                            pageAnimation={pageAnimation}
+                            openedPage={openedPage}
+                            setOpenedPage={setOpenedPage}
+                        />
+                    ))}
+                </S.PageCont>
+            </S.MobileView>
+        </S.CardPaging>
+    )
+}
+
+const pageContVariant: Variants = {
+    openPage: {
+        height: '94%',
+        width: '90%'
+    },
+    closePage: {
+        width: '70%',
+        height: '50%'
+    }
+}
+
+export default CardPaging;
