86dd1ccbdb84ea3d5a80f8afaaafd4a7f3b3a8d5
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-22 15:24:38 +0800

    💫✨Create list shuffle animation

---
+++ src/components/listShuffler/ListShuffler.style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import {motion} from 'framer-motion';
+
+export const ListCont = styled(motion.ul)`
+    border: 2px solid turquoise;
+    border-radius: 5px;
+    padding: 40px;
+    width: 90%;
+    max-width: 500px;
+    margin: 0 auto;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    cursor: pointer;
+`;
+
+export const List = styled(motion.li)<{num: number}>`
+    list-style-type: none;
+    font-size: 200%;
+    color: white;
+    text-align: center;
+    padding: 5px;
+    margin: 3px 0;
+    background-color: ${({num: key}) => {
+        switch(key) {
+            case 0:
+                return 'turquoise';
+            case 1:
+                return 'hotpink';
+            case 2: 
+                return 'coral';
+            case 3:
+                return 'crimson';
+            case 4:
+                return '#ee6f57';
+            case 5:
+                return '#835858';
+        }
+    }};
+`;
+
+export const Cont = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`;
---
+++ src/components/listShuffler/ListShuffler.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+import * as S from './ListShuffler.style';
+
+function App() {
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30
+  };
+  const [list, setList] = useState([
+    {
+      num: 0,
+      content: 'Zero'
+    },
+    {
+      num: 1,
+      content: 'One'
+    },
+    {
+      num: 2,
+      content: 'Two'
+    },
+    {
+      num: 3,
+      content: 'Three'
+    },
+    {
+      num: 4,
+      content: 'Four'
+    },
+    {
+      num: 5,
+      content: 'Five'
+    }
+  ]);
+
+
+  function handleSwitch() {
+    const copy = list.slice(0);
+    shuffleArray(copy);
+    setList(copy);
+  }
+
+  function shuffleArray(array: any[]) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  const lists = list.map(list => (
+    <S.List layout transition={spring} num={list.num} key={list.num}>
+      {list.content}
+    </S.List>
+  ));
+
+  return (
+    <S.Cont>
+      <S.ListCont onClick={handleSwitch}>
+        {lists}
+      </S.ListCont>
+    </S.Cont>
+  )
+}
+
+export default App;
