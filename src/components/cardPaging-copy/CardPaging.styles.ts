14e9ad130815d6aec98a91b8b43e9c271e028713
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-23 20:32:45 +0800

    💄update styles

--- src/components/cardPaging-copy/CardPaging.styles.ts
+++ src/components/cardPaging-copy/CardPaging.styles.ts
@@ -32,8 +32,7 @@ export const MobileView = styled.div`
 export const PageCont = styled(motion.div)`
     display: flex;
     flex-direction: row;
-    overflow-x: hidden;
-    overflow-y: visible;
+    overflow: hidden;
     width: 70%;
     height: 50%;
     cursor: grab;
@@ -47,11 +46,29 @@ export const Carousel = styled.div`
     height: 70%;
 `;
 
-export const Page = styled(motion.div)<{bgColor: string}>`
-    background-color: ${({bgColor}) => bgColor};
+export const PageWrapper = styled(motion.div)`
     width: 100%;
     height: 100%;
     margin: 0 auto;
     flex: 0 0 auto;
     border-radius: 20px;
+`;
+
+export const Page = styled(motion.div)<{bgColor: string}>`
+    display: flex;
+    flex-direction: column-reverse;
+    background-color: ${({bgColor}) => bgColor};
+    height: 100%;
+    width: 100%;
+    border-radius: 20px;
+`;
+
+export const SwipeSvg = styled.svg<{hide: boolean}>`
+    display: ${({hide}) => hide ? 'none' : 'block'};
+    width: 10%;
+    margin: 30px auto;
+`;
+
+export const Swipe = styled.polygon`
+    opacity: .6;
 `;
