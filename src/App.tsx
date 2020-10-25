110cf1bba307d1ec0c5d7b4066e8888e07bfc1fa
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-25 16:53:13 +0800

    💩Mema

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import {useViewportScroll} from 'framer-motion';
+import {Switch, Route, Link} from 'react-router-dom';
 
 import * as S from './App.styles';
-import { useViewportScroll, useDomEvent } from 'framer-motion';
+import Typing from './components/typing/Typing';
+import TwitterLike from './components/twitterLike/TwitterLike';
+import ToggleMenu from './components/toggleMenu/ToggleMenu';
+import PeekGallery from './components/peekGallery/PeekGallery';
+import ListShuffler from './components/listShuffler/ListShuffler';
+import DropLets from './components/droplets/Dropslets';
+import ChatHead from './components/chatHeads/ChatHeads';
+import CardPaging from './components/cardPaging/CardPaging';
 
 const colors = ['#2d6187', '#effad3', '#a8dda8', '#ff9642', '#ffe05d', '#d9e4dd'];
 
@@ -32,4 +41,63 @@ function App() {
     )
 }
 
-export default App;
+function PrototypeApp() {
+    return (
+        <>
+            <Switch>
+                <Route path='/typing'>
+                    <Typing />
+                </Route>
+                <Route path='/twitter-like'>
+                    <TwitterLike />
+                </Route>
+                <Route path='/toggle-menu'>
+                    <ToggleMenu />
+                </Route>
+                <Route path='/peek-gallery'>
+                    <PeekGallery />
+                </Route>
+                <Route path='/list-shuffler'>
+                    <ListShuffler />
+                </Route>
+                <Route path='/droplets'>
+                    <DropLets />
+                </Route>
+                <Route path='/chat-heads'>
+                    <ChatHead />
+                </Route>
+                <Route path='/card-paging'>
+                    <CardPaging />
+                </Route>
+                <Route path='/'>
+                    <Link to='/typing' >
+                        Typing
+                    </Link><br />
+                    <Link to='/peek-gallery'>
+                        Peek Gallery
+                    </Link><br />
+                    <Link to='/chat-heads'>
+                        Chatheads
+                    </Link><br />
+                    <Link to='/card-paging'>
+                        Card Paging
+                    </Link><br />
+                    <Link to='/twitter-like' >
+                        An attempt to copy twitter like animation
+                    </Link><br />
+                    <Link to='/toggle-menu'>
+                        Add Message
+                    </Link><br />
+                    <Link to='/list-shuffler'>
+                        List Shuffler
+                    </Link><br />
+                    <Link to='/droplets'>
+                        Droplet
+                    </Link><br />
+                </Route>
+            </Switch>
+        </>
+    )
+}
+
+export default PrototypeApp;
