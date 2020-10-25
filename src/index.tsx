b4c091bc4911ba2a3b05fe410db6ffe17fdf808a
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-25 16:53:55 +0800

    🚚Direct to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import './cssGlobal.css';
-import App from './components/cardPaging-copy/CardPaging';
 // import App from './components/testing/Testing';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
