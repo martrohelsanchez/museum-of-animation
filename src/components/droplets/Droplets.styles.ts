8e27e67669866530f6518453f4130092565ec8e3
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-26 18:24:26 +0800

    🔥Remove ThemeProvider and move lift it up on App

--- src/components/droplets/Droplets.styles.ts
+++ src/components/droplets/Droplets.styles.ts
@@ -3,8 +3,8 @@ import {motion} from 'framer-motion';
 
 export const Bg = styled.div`
     background-color: black;
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
 `;
 
 export const FirstWave = styled(motion.div)`
