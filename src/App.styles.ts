29406be74aebc448e856e15f07445a62a871c093
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-26 18:16:01 +0800

    💄Remove bgColor

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
+import {motion} from 'framer-motion';
 
-export const Section = styled.section<{bgColor: string}>`
-    background-color: ${({bgColor}) => bgColor};
-    width: 100vw;
+export const Section = styled(motion.section)`
+    width: 100%;
     height: 100vh;
-`;
+`
