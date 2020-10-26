f9fe4cad24e3d2dcc121d33d439481ac0981c437
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-26 18:16:51 +0800

    🔥Remove react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {Reset} from 'styled-reset';
 
 import './cssGlobal.css';
-// import App from './components/testing/Testing';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+      <Reset />
       <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
