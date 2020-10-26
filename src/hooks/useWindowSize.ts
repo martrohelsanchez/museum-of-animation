cd5fe44cbcc0dc45775a4a2e003b9bfd983e5186
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-26 18:18:29 +0800

    🎨Return {width, height} instead of array

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,10 +1,14 @@
 import {useState, useLayoutEffect} from 'react';
 
 function useWindowSize() {
-    const [size, setSize] = useState([0, 0]);
+    // const [size, setSize] = useState([0, 0]);
+    const [size, setSize] = useState({width: 0, height: 0});
     useLayoutEffect(() => {
         function updateSize() {
-            setSize([window.innerWidth, window.innerHeight]);
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
         }
         window.addEventListener('resize', updateSize);
         updateSize();
