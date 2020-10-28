9603def0923d0d0670241c75107d45022939498b
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-28 12:49:51 +0800

    💄hide overflow

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -4,4 +4,6 @@ import {motion} from 'framer-motion';
 export const Section = styled(motion.section)`
     width: 100%;
     height: 100vh;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    overflow: hidden;
 `
