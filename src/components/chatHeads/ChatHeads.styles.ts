ba55ee0f7c6f80bb1c41f7d3dfa089fb9b6f5bdd
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-29 18:10:56 +0800

    💄Update styling

--- src/components/chatHeads/ChatHeads.styles.ts
+++ src/components/chatHeads/ChatHeads.styles.ts
@@ -5,12 +5,23 @@ export const Bg = styled.div`
     background-color: #28abb9;
     width: 100%;
     height: 100%;
+    overflow: hidden;
 `;
 
-export const ChatHead = styled(motion.div)<{color: string}>`
+export const ChatHead = styled(motion.div)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     position: absolute;
-    background-color: ${({color}) => color};
     height: 60px;
     width: 60px;
     border-radius: 50%;
+    cursor: grab;
+    touch-action: none;
 `
+
+export const UserImg = styled.img`
+    height: 90%;
+    width: 90%;
+    border-radius: 50%;
+`;
