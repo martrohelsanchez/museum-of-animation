16fc961f8fb741f1e6502398d4930070812ae728
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-11-02 17:16:13 +0800

    💫✨Create Christmas card animation

---
+++ src/components/christmasCard/ChristmasCard.style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import {Bg as bg} from '../../shared/styles';
+
+export const Bg = styled(bg)`
+    background-color: #f4f4f2;
+`;
---
+++ src/components/christmasCard/ChristmasCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './ChristmasCard.style';
+
+function ChristmasCard() {
+    return (
+        <S.Bg>
+            
+        </S.Bg>
+    );
+}
+
+export default ChristmasCard;
