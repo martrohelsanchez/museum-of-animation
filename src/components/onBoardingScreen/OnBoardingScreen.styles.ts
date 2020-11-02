eb15427362f9f25966d849d677c2385fe5ae157e
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-11-02 17:14:32 +0800

    💄Update style

--- src/components/onBoardingScreen/OnBoardingScreen.styles.ts
+++ src/components/onBoardingScreen/OnBoardingScreen.styles.ts
@@ -1,27 +1,58 @@
 import styled from 'styled-components';
 import {motion} from 'framer-motion';
 
-export const Bg = styled(motion.div)<{bgColor: string}>`
+import {Bg as bg, ParentSize, CenterHorVer} from '../../shared/styles'
+
+export const Bg = styled(bg)<{bgColor: string}>`
     background-color: ${({bgColor}) => bgColor};
     display: flex;
     justify-content: center;
     align-items: flex-end;
-    width: 100%;
-    height: 100%;
     padding: 50px;
     box-sizing: border-box;
-    overflow: hidden;
 `;
 
-export const NavCircle = styled(motion.div)`
-    background-color: #a3d8f4;
+export const NavCircle = styled(motion.div)<{bgColor: string}>`
+    position: relative;
+    background-color: ${({bgColor}) => bgColor};
     width: 100px;
     height: 100px;
     border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    will-change: transform;
+    cursor: grab;
+
+    &:active {
+        cursor: grabbing;
+    }
 `;
 
-export const Grab = styled(NavCircle)`
+export const Grab = styled(motion.div)`
+    ${ParentSize};
+    position: absolute;
+    top: 0;
+    left: 0;
     background-color: transparent;
-    width: 100%;
-    height: 100%;
+    border-radius: 50%;
+    z-index: 10;
 `;
+
+export const NextColorCont = styled(Grab)`
+    ${CenterHorVer};
+    position: absolute;
+    top: 0;
+    left: 0;
+`;
+
+export const NextColor = styled(Grab)<{bgColor: string}>`
+    background-color: ${({bgColor}) => bgColor};
+`;
+
+export const Svg = styled(motion.svg)`
+    ${ParentSize};
+    transform: rotate(90deg);
+`;
+
+export const Arrow = styled.polygon``;
