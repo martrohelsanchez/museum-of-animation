34c834b978a49fcc9edc66a8c46eb7256f788912
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-11-03 19:45:55 +0800

    💫✨Open the christmas card when user clicks it

--- src/components/christmasCard/ChristmasCard.tsx
+++ src/components/christmasCard/ChristmasCard.tsx
@@ -1,14 +1,28 @@
-import React, { useRef, useEffect } from 'react';
-import {useElementScroll, motion, useTransform} from 'framer-motion';
+import React, { useRef, useEffect, useState } from 'react';
+import {useElementScroll, motion, useTransform, Variants} from 'framer-motion';
 
+import useWindowSize from '../../hooks/useWindowSize';
 import * as S from './ChristmasCard.style';
 import envelopeBack from '../../images/envelope-back.svg';
 import envelopeFront from '../../images/envelope-front.svg';
 import envelopeFlap from '../../images/envelope-flap.svg';
 import christmasCardBack from '../../images/christmas-card-back.svg';
-import useWindowSize from '../../hooks/useWindowSize';
+import christmasCardFront from '../../images/christmas-card-front.svg';
+
+const flapOpen = 0.25;
+const rotateRight = 0.48;
+const moveEnvLeftStart = 0.5
+const rotateLeft = 0.7;
+const moveEnvLeftEnd = 0.96;
+
+const cardSpring = {
+    type: 'spring',
+    stiffness: 40
+}
 
 function ChristmasCard() {
+    const [cardIsOpen, setCardIsOpen] = useState(false);
+
     const windowSize = useWindowSize();
     const bgRef = useRef<HTMLDivElement>(null);
     const envRef = useRef<HTMLDivElement>(null!);
@@ -18,50 +32,45 @@ function ChristmasCard() {
     const envWidth = envRef.current?.getBoundingClientRect().width || 0;
     const christmasCardHeight = envRef.current ? envRef.current.getBoundingClientRect().height * 0.90 : 0;
 
-    const envFlapRotateX = useTransform(scrollYProgress, [0, .15, 0.4, 0.76], [0, 180, 180, 0]);
-    const envRotate = useTransform(scrollYProgress, [0.15, 0.32, 0.6], [0, 20, 0]);
-    const envY = useTransform(scrollYProgress, [0, 0.8], [0, getScrollTopEnd(bgRef) * 0.8]);
-    const envX = useTransform(scrollYProgress, [0.4, 0.83], [0, -(windowSize.width / 2 + envWidth / 2)]);
-
-    const christmasCardY = useTransform(scrollYProgress, [0.15, 0.32, 0.6], [0, -(christmasCardHeight + 50), 0]);
+    const envFlapRotateX = useTransform(scrollYProgress, [0, flapOpen, moveEnvLeftStart, moveEnvLeftEnd - 0.09], [0, 180, 180, 0]);
+    const envRotate = useTransform(scrollYProgress, [flapOpen, rotateRight, rotateLeft], [0, 20, 0]);
+    const envY = useTransform(scrollYProgress, [0, moveEnvLeftEnd], [0, getScrollTopEnd(bgRef) * moveEnvLeftEnd]);
+    const envX = useTransform(scrollYProgress, [moveEnvLeftStart, moveEnvLeftEnd], [0, -(windowSize.width / 2 + envWidth / 2)]);
+    const christmasCardY = useTransform(scrollYProgress, [flapOpen, rotateRight, rotateLeft], [0, -(christmasCardHeight + 50), 0]);
 
     useEffect(() => {
         scrollYProgress.onChange(y => {
-            if (y > 0.15 && envFlapRef.current.style.zIndex !== '3') {
+            if (y > flapOpen && envFlapRef.current.style.zIndex !== '3') {
                 envFlapRef.current.style.zIndex = '3';
             }
-            if (y < 0.15 && envFlapRef.current.style.zIndex === '3') {
+            if (y < flapOpen && envFlapRef.current.style.zIndex === '3') {
                 envFlapRef.current.style.zIndex = '6';
             }
-            if (y > 0.32 && envFront.current.style.zIndex !== '2') {
-                console.log('asdfsd')
+            if (y > rotateRight && envFront.current.style.zIndex !== '2') {
                 envFront.current.style.zIndex = '2';
             }
-            if (y < 0.32 && envFront.current.style.zIndex === '2') {
+            if (y < rotateRight && envFront.current.style.zIndex === '2') {
                 envFront.current.style.zIndex = '5';
             }
         });
     }, [])
+
+    function onCardClick() {
+        setCardIsOpen(!cardIsOpen);
+    }
 
     return (
         <S.Bg
             ref={bgRef}
         >
             <S.StackElements
+                as={motion.div}
                 ref={envRef}
                 style={{
                     rotate: envRotate,
                     y: envY,
-                    // x: envX
                 }}
             >
-                {/* <S.EnvelopeCont
-                    style={{
-                        rotate: envRotate,
-                        y: envY,
-                        x: envX
-                    }}
-                > */}
                     <S.EnvelopeBack src={envelopeBack} 
                         style={{
                             x: envX
@@ -73,12 +82,37 @@ function ChristmasCard() {
                             x: envX
                         }}
                     />
-                    <S.ChristmasCard 
-                        src={christmasCardBack} 
+                    <S.CardCont 
+                        onClick={onCardClick}
                         style={{
-                            y: christmasCardY
+                            y: christmasCardY,
                         }}
-                    />
+                        transition={cardSpring}
+                        animate={cardIsOpen ? 'open' : 'close'}
+                        variants={cardVariant}
+                    >
+                        <S.StackCards>
+                            <S.CardLeftBack 
+                                src={christmasCardBack} 
+                                variants={cardLeftBackVariant}
+                                style={{
+                                    perspective: 1500
+                                }}
+                                transition={cardSpring}
+                            />
+                            <S.CardLeftFront 
+                                src={christmasCardFront} 
+                                variants={cardLeftFrontVariant}
+                                style={{
+                                    perspective: 1500
+                                }}
+                                transition={cardSpring}
+                            />
+                            <S.CardRightFront
+                                src={christmasCardFront}
+                            />
+                        </S.StackCards>
+                    </S.CardCont>
                     <S.EnvelopeFlap
                         ref={envFlapRef}
                         src={envelopeFlap} 
@@ -89,11 +123,43 @@ function ChristmasCard() {
                             x: envX
                         }}
                     />
-                {/* </S.EnvelopeCont> */}
             </S.StackElements>
             <S.Pampahaba></S.Pampahaba>
         </S.Bg>
     );
+}
+
+const cardLeftBackVariant: Variants = {
+    close: {
+        rotateX: 10,
+        zIndex: 5
+    },
+    open: {
+        rotateX: 180,
+        zIndex: 4
+    }
+}
+
+const cardLeftFrontVariant: Variants = {
+    close: {
+        rotateX: 10,
+        zIndex: 4
+    },
+    open: {
+        rotateX: 180,
+        zIndex: 5
+    }
+}
+
+const cardVariant: Variants = {
+    close: {
+        rotate: 0,
+        x: 0
+    },
+    open: {
+        rotate: -90,
+        x: '50%'
+    }
 }
 
 function getScrollTopEnd(ref: React.RefObject<HTMLDivElement>) {
