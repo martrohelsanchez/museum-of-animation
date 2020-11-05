c04983430f49a2e312b8581ffd1f78f89996512c
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-11-05 10:55:37 +0800

    💄Remove shadows when in mobile and optimize transform property

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -3,7 +3,14 @@ import {motion} from 'framer-motion';
 
 export const Section = styled(motion.section)`
     width: 100%;
-    height: 100vh;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    height: 100%;
+    box-shadow: 0 0px 20px 0 rgba(50, 50, 73, 0.4);
     overflow: hidden;
+    will-change: transform;
+
+    @media all and (max-width: ${({ theme }) => theme.mobile}) {
+        & {
+            box-shadow: none;
+        }
+    }
 `
